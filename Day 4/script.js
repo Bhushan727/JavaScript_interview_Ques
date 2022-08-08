7e315dc3a2b2241f9365dee9e7ffd3bae6f0cab4
shashi Bhushan <bhushans727@gmail.com>
2022-08-09 00:03:45 +0530

    added day 4 Questions

---
+++ Day 4/script.js
@@ -0,0 +1,19 @@
+
+// Ques 1
+// Temporal Dead Zone - Temporal Dead zone (TDZ) is the area of black where a  variable is inaccessible until the momentcomputer fully initaializes it with a vlaue.
+//      In other words we can say that temporal Dead Zone (TDZ) is state of a variable which are unreachable. They are in the scope but not declared.
+
+
+// Ques 2.
+// the for in loop is basically for iterating the keys of an object
+
+// for(let key in Student)
+// {
+//     console.log(`${key} = ${Student[key]}`);
+// }
+
+
+// Ques 3.
+// Explain Local Scope, Block Scope, Functional Scope and Scope Chain in javascript
+
+
