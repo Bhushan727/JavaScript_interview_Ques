c2e1bcbc9aa4de5f3d2881c67087b22ed186ff13
shashi Bhushan <bhushans727@gmail.com>
2022-08-12 21:14:40 +0530

    modified day 5 code

--- Day 5/script.js
+++ Day 5/script.js
@@ -43,7 +43,6 @@ console.log(`number of 0's in given array is ${count}`);
 
 
 
-
 let array = [10,11,21,40,23,22,13,30,10,41];
 let countEven = 0;
 let countOdd = 0;
@@ -84,10 +83,37 @@ for(let i=0; i<str.length; i++)
 console.log(`total number of vowel in given String is ${counts}`);
 
 
+// Ques
+// Write a code to create two objects with 2 properties each, one will be string and other will be an array. Create a function to check if all the elements of the arrays in both the objects are same
 
+let empOne = {
+    id : "phase1",
+    products : ['apple', 'samsung', 'oneplus', 'Nokia']
+}
 
+let empTwo = {
+    id : "phase2",
+    products : ['apple', 'samsung', 'oneplus', 'Nokia']
+}
 
+console.log(empOne.products);
+let counted = 0;
+let i;
 
+for(i=0; i<empOne.products.length; i++)
+{
+    if(empOne.products[i] === empTwo.products[i])
+    {
+        counted++;
+    }
+}
 
+if(i == counted)
+{
+    console.log("Elements in array of both the objects are same");
+}
+else{
+    console.log("not same");
+}
 
 
